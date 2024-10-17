c9d8751607c319c8c9be6adb11e9a8c1fb156108
Ally Byers <allybyers@gmail.com>
2024-10-17 11:55:05 +0200

    Updated settings.py

--- my_scrapy_project/settings.py
+++ my_scrapy_project/settings.py
@@ -1,39 +1,30 @@
 # Scrapy settings for my_scrapy_project project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://docs.scrapy.org/en/latest/topics/settings.html
-#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
 BOT_NAME = "my_scrapy_project"
 
 SPIDER_MODULES = ["my_scrapy_project.spiders"]
 NEWSPIDER_MODULE = "my_scrapy_project.spiders"
 
-
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "my_scrapy_project (+http://www.yourdomain.com)"
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 16
 
 # Configure a delay for requests for the same website (default: 0)
-# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
-# The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+DOWNLOAD_DELAY = 0.5  # Adjust to balance speed and server load
+CONCURRENT_REQUESTS_PER_DOMAIN = 8  # Concurrent requests for the same domain
+CONCURRENT_REQUESTS_PER_IP = 8  # Concurrent requests per IP
 
-# Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+# Retry failed requests
+RETRY_ENABLED = True
+RETRY_TIMES = 5  # Number of retries
+RETRY_HTTP_CODES = [500, 502, 503, 504, 403, 408]
 
-# Disable Telnet Console (enabled by default)
+# Enable or disable Telnet Console
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
@@ -43,111 +34,67 @@ ROBOTSTXT_OBEY = True
 #}
 
 # Enable or disable spider middlewares
-# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    "my_scrapy_project.middlewares.MyScrapyProjectSpiderMiddleware": 543,
-#}
+SPIDER_MIDDLEWARES = {
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
 
 # Enable or disable downloader middlewares
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "my_scrapy_project.middlewares.MyScrapyProjectDownloaderMiddleware": 543,
-#}
-
-# Enable or disable extensions
-# See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
-#    "scrapy.extensions.telnet.TelnetConsole": None,
-#}
-
-# Configure item pipelines
-# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "my_scrapy_project.pipelines.MyScrapyProjectPipeline": 300,
-#}
-
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
-
-# Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = "httpcache"
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
-
-# Set settings whose default value is deprecated to a future-proof value
-REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
-FEED_EXPORT_ENCODING = "utf-8"
-
-# Enable Media Pipelines
-ITEM_PIPELINES = {
-    'scrapy.pipelines.images.ImagesPipeline': 1,
-    'scrapy.pipelines.files.FilesPipeline': 2,
-}
-ITEM_PIPELINES = {
-    'my_scrapy_project.pipelines.S3Pipeline': 300,
-}
-
-# Set Media URL paths
-IMAGES_STORE = 'images'
-FILES_STORE = 'files'
-
-# Add these settings to force Scrapy to use modern TLS settings
-DOWNLOADER_CLIENT_TLS_METHOD = 'TLS'
-DOWNLOADER_CLIENT_TLS_VERBOSE_LOGGING = True
-
-# Disable deprecated SSL protocols (like SSLv3 and TLSv1) to avoid security issues
-DOWNLOADER_CLIENT_TLS_CIPHERS = 'ECDHE+AESGCM:ECDHE+CHACHA20:ECDHE+SHA256:!SSLv3:!TLSv1:!TLSv1.1'
-
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-
-RETRY_ENABLED = True
-RETRY_TIMES = 5  # Number of retries
-RETRY_HTTP_CODES = [500, 502, 503, 504, 403]  # Include 403 in the retry codes
-
 DOWNLOADER_MIDDLEWARES = {
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
     'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 1,
     'scrapy_proxy_pool.middlewares.ProxyPoolMiddleware': 100,
     'scrapy_proxy_pool.middlewares.BanDetectionMiddleware': 200,
 }
 
-# Enable retrying failed HTTP requests
-RETRY_ENABLED = True
-RETRY_TIMES = 10
-RETRY_HTTP_CODES = [500, 502, 503, 504, 403, 408]
+# Enable item pipelines for media
+ITEM_PIPELINES = {
+    'scrapy.pipelines.images.ImagesPipeline': 1,
+    'scrapy.pipelines.files.FilesPipeline': 2,
+    'my_scrapy_project.pipelines.S3Pipeline': 300,
+}
 
 # Enable Scrapy Proxy Pool
 PROXY_POOL_ENABLED = True
 
-DEPTH_LIMIT = 5  # Scrapy will follow links up to 5 levels deep
+# Set Media URL paths
+IMAGES_STORE = 'images'
+FILES_STORE = 'files'
 
-DEPTH_PRIORITY = 1  # Prioritizes deeper pages over shallow ones
+# HTTP Cache settings
+HTTPCACHE_ENABLED = True
+HTTPCACHE_EXPIRATION_SECS = 0
+HTTPCACHE_DIR = 'httpcache'
+HTTPCACHE_IGNORE_HTTP_CODES = [500, 502, 503, 504, 403, 404]
+HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 
-DOWNLOADER_MIDDLEWARES = {
-    'scrapy_splash.SplashCookiesMiddleware': 723,
-    'scrapy_splash.SplashMiddleware': 725,
-    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
-}
+# TLS settings for Scrapy
+DOWNLOADER_CLIENT_TLS_METHOD = 'TLS'
+DOWNLOADER_CLIENT_TLS_VERBOSE_LOGGING = True
+DOWNLOADER_CLIENT_TLS_CIPHERS = 'ECDHE+AESGCM:ECDHE+CHACHA20:ECDHE+SHA256:!SSLv3:!TLSv1:!TLSv1.1'
 
-SPIDER_MIDDLEWARES = {
-    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
-}
-
+# Splash settings
+SPLASH_URL = 'https://splash-service-rfid.onrender.com'
 DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
 HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
 
-SPLASH_URL =  'https://splash-service-rfid.onrender.com'
+# Limit the depth of scraping
+DEPTH_LIMIT = 5  # Adjust based on how deep you want to scrape
 
+# Prioritize deeper requests
+DEPTH_PRIORITY = 1
 
+# Set download timeout to avoid hanging
+DOWNLOAD_TIMEOUT = 15
+
+# Auto-throttling settings
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_START_DELAY = 1  # Initial delay for requests
+AUTOTHROTTLE_MAX_DELAY = 10  # Max delay in case of high latencies
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0  # Number of requests in parallel
+AUTOTHROTTLE_DEBUG = False
+
+# Set settings whose default value is deprecated to a future-proof value
+REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
+FEED_EXPORT_ENCODING = "utf-8"
